b0b0ad2eb81ebe1d93b490c8485b79b5e3464b01
Pyrite <timd1919@gmail.com>
2022-07-26 18:41:57 -0500

    Added basic header and fixed express static resources.

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 require('dotenv').config()
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
@@ -18,9 +14,14 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName)
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get("/", async (req, res) => {
     const jobList = await db.collection("JobBase").find().toArray()
-    res.render('index.ejs', { jobs: jobList})
+    res.render('index.ejs', { jobs: jobList })
 })
 
 app.listen(process.env.PORT || PORT, () => {
