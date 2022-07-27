d02a851a0a923d1c850e0a093e4a730570120a67
Pyrite <timd1919@gmail.com>
2022-07-27 01:47:04 -0500

    Fixed get

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 require('dotenv').config()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'JobBase'
+    dbName = 'jobBase'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
@@ -14,14 +18,20 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName)
     })
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+let date = new Date();
 
 app.get("/", async (req, res) => {
-    const jobList = await db.collection("JobBase").find().toArray()
-    res.render('index.ejs', { jobs: jobList })
+    const jobList = await db.collection("jobBase").find().toArray()
+    res.render('index.ejs', { job: jobList })
+})
+
+app.post('/addJob', (req, res) => {
+    db.collection('jobBase').insertOne({ job: req.body.jobItem, date: date.getDate() })
+        .then(result => {
+            console.log('Job Added')
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
 })
 
 app.listen(process.env.PORT || PORT, () => {
