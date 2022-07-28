2efdce362d21e016cb4d28e2347a40d93b656ab1
Pyrite <timd1919@gmail.com>
2022-07-28 01:33:50 -0500

    Heroku fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'jobBase'
+    dbName = 'JobBase'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
