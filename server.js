0e3623cff2b42c0e92c807f4bc0df788bcf47004
Pyrite <timd1919@gmail.com>
2022-07-28 05:21:12 -0500

    Undo Heroku hack

--- server.js
+++ server.js
@@ -12,42 +12,38 @@ let db,
     dbConnectionStr = process.env.MONGO_STRING,
     dbName = 'jobBase'
 
-async function main() {
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
 
-    await MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-        .then(client => {
-            console.log(`Connected to ${dbName} Database`)
-            db = client.db(dbName)
+let date = new Date();
+
+app.get("/", async (req, res) => {
+    const jobList = await db.collection("jobBase").find().toArray()
+    res.render('index.ejs', { job: jobList })
+})
+
+app.post('/addJob', (req, res) => {
+    db.collection('jobBase').insertOne({ job: req.body.jobItem, dateDay: date.getDate(), dateMonth: date.getMonth(), dateYear: date.getFullYear() })
+        .then(result => {
+            console.log('Job Added')
+            res.redirect('/')
         })
+        .catch(error => console.error(error))
+})
 
-    let date = new Date();
+app.delete('/deleteItem', (req, res) => {
+    db.collection('jobBase').deleteOne({ job: req.body.jobFromJS })
+        .then(result => {
+            console.log('Job Deleted')
+            res.json('Job Deleted')
+        })
+        .catch(error => console.error(error))
 
-    app.get("/", async (req, res) => {
-        const jobList = await db.collection("jobBase").find().toArray()
-        res.render('index.ejs', { job: jobList })
-    })
-
-    app.post('/addJob', (req, res) => {
-        db.collection('jobBase').insertOne({ job: req.body.jobItem, dateDay: date.getDate(), dateMonth: date.getMonth(), dateYear: date.getFullYear() })
-            .then(result => {
-                console.log('Job Added')
-                res.redirect('/')
-            })
-            .catch(error => console.error(error))
-    })
-
-    app.delete('/deleteItem', (req, res) => {
-        db.collection('jobBase').deleteOne({ job: req.body.jobFromJS })
-            .then(result => {
-                console.log('Job Deleted')
-                res.json('Job Deleted')
-            })
-            .catch(error => console.error(error))
-
-    })
+})
 }
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-main();
