4f8ec554d45134f56705af09cdae295a62984615
Pyrite <timd1919@gmail.com>
2022-07-28 05:17:23 -0500

    Heroku final fix

--- server.js
+++ server.js
@@ -2,50 +2,52 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
+require("dotenv").config();
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-
 let db,
     dbConnectionStr = process.env.MONGO_STRING,
     dbName = 'jobBase'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+async function main() {
+
+    await MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+        .then(client => {
+            console.log(`Connected to ${dbName} Database`)
+            db = client.db(dbName)
+        })
+
+    let date = new Date();
+
+    app.get("/", async (req, res) => {
+        const jobList = await db.collection("jobBase").find().toArray()
+        res.render('index.ejs', { job: jobList })
     })
 
-let date = new Date();
+    app.post('/addJob', (req, res) => {
+        db.collection('jobBase').insertOne({ job: req.body.jobItem, dateDay: date.getDate(), dateMonth: date.getMonth(), dateYear: date.getFullYear() })
+            .then(result => {
+                console.log('Job Added')
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+    })
 
-app.get("/", async (req, res) => {
-    const jobList = await db.collection("jobBase").find().toArray()
-    res.render('index.ejs', { job: jobList })
-})
+    app.delete('/deleteItem', (req, res) => {
+        db.collection('jobBase').deleteOne({ job: req.body.jobFromJS })
+            .then(result => {
+                console.log('Job Deleted')
+                res.json('Job Deleted')
+            })
+            .catch(error => console.error(error))
 
-app.post('/addJob', (req, res) => {
-    db.collection('jobBase').insertOne({ job: req.body.jobItem, dateDay: date.getDate(), dateMonth: date.getMonth(), dateYear: date.getFullYear() })
-        .then(result => {
-            console.log('Job Added')
-            res.redirect('/')
-        })
-        .catch(error => console.error(error))
-})
-
-app.delete('/deleteItem', (req, res) => {
-    db.collection('jobBase').deleteOne({ job: req.body.jobFromJS })
-        .then(result => {
-            console.log('Job Deleted')
-            res.json('Job Deleted')
-        })
-        .catch(error => console.error(error))
-
-})
+    })
+}
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+main();
